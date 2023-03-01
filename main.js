5f755eda7d51d7616399ad48f82b31ffb74a26c5
Kayathri2218 <kaya3kayu2000@gmail.com>
2023-03-02 03:32:37 +0530

    count digit the array in string or number.

---
+++ main.js
@@ -0,0 +1,8 @@
+//2.Design a method to count how many digits in the array (digits not number). 
+
+let digi = ['Manimegalai', 'Karthika', 'Manju', 'Mahesh'];
+let a = [];
+for (i = 0; i < digi.length; i++) {
+    a[i] = digi[i].length;
+}
+console.log(a);
